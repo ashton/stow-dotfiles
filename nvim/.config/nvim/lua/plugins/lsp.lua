63b5c059b599fbd7232ba2d835a49f82a1a214c7
Matheus Ashton Silva <matheusashton@gmail.com>
2025-11-25 16:56:17 -0300

    [nvim] lsp update, plugin-dev plugins

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,48 +1,6 @@
-local format_au_group = vim.api.nvim_create_augroup("LspFormat", { clear = true })
-
-local function get_extra_config(language_server)
-  local config_table = {
-    elixirls = {
-      settings = {
-        dialyzerEnabled = true,
-        fetchDeps = true,
-        enableTestLenses = true,
-        suggestSpecs = true,
-      }
-    },
-    rescriptls = {
-      init_options = {
-        extensionConfiguration = {
-          askToStartBuild = true,
-          allowBuiltInFormatter = true,
-          signatureHelp = {
-            enabled = true
-          }
-        }
-      },
-      settings = {
-        codeLens = true,
-        autoRunCodeAnalysis = true,
-      }
-    },
-  }
-
-  return config_table[language_server] or {}
-end
-
-
-
 local function on_attach_chain(client, bufnr)
   -- autocmd to format buffers with LSP on save
-  vim.api.nvim_clear_autocmds({ group = format_au_group, buffer = bufnr })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = format_au_group,
-    buffer = bufnr,
-    callback = function()
-      vim.lsp.buf.format()
-    end,
-  })
-
+  --
   require("lsp_signature").on_attach({
     floating_window = false,
     hint_enable = false
@@ -69,18 +27,6 @@ local function create_capabilities()
 end
 
 
-local function config_server(server_name)
-  local settings = get_extra_config(server_name)
-  local lsp_capabilities = create_capabilities()
-  local on_attach = { on_attach = on_attach_chain }
-
-  require 'lspconfig'[server_name].setup(vim.tbl_extend("error",
-    settings,
-    lsp_capabilities,
-    on_attach
-  ))
-end
-
 return {
   { "williamboman/mason.nvim", lazy = true, config = true },
   {
@@ -89,18 +35,18 @@ return {
     event = "LspAttach",
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
     dependencies = {
-      "williamboman/mason.nvim",
+      { "mason-org/mason.nvim", opts = {} },
       {
         "neovim/nvim-lspconfig",
         cmd = { "LspInfo", "LspInstall", "LspStart" },
         event = { "BufReadPre", "BufNewFile" },
         dependencies = {
-          {
-            "ray-x/lsp_signature.nvim",
-            event = "InsertEnter",
-          },
           { "hrsh7th/cmp-nvim-lsp" },
           {
             "antosha417/nvim-lsp-file-operations",
@@ -108,7 +54,33 @@ return {
               "nvim-lua/plenary.nvim",
               "nvim-neo-tree/neo-tree.nvim",
             },
-            config = true
+            config = function()
+              vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('custom.lsp', {}),
+                callback = function(args)
+                  local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+                  -- Auto format buffer
+                  if not client:supports_method('textDocument/willSaveWaitUntil') and client:supports_method('textDocument/formatting') then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                      group = vim.api.nvim_create_augroup('custom.lsp', { clear = false }),
+                      buffer = args.buf,
+                      callback = function()
+                        vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                      end,
+                    })
+                  end
+                end
+              })
+
+              vim.lsp.config('*', {
+                capabilities = create_capabilities(),
+                on_attach = on_attach_chain
+              })
+
+              vim.lsp.enable('gleam')
+              vim.lsp.enable('ocamllsp')
+            end
           },
           {
             "SmiteshP/nvim-navic",
@@ -133,20 +105,7 @@ return {
         -- "nextls",
         "yamlls",
       },
-      ensure_configured = {
-        "gleam",
-        "ocamllsp"
-      },
-      handlers = {
-        config_server
-      }
+      automatic_enable = true
     },
-    config = function(_, opts)
-      require('mason-lspconfig').setup(opts)
-
-      for _, server_name in pairs(opts.ensure_configured) do
-        config_server(server_name)
-      end
-    end
   },
 }

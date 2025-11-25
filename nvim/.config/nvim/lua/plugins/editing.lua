aa28e58235e2a6877d9ad886d856774dd633303d
Matheus Ashton <matheusashton@gmail.com>
2025-11-25 16:13:15 -0300

    [nvim] updating keymapping and float windows diagnostics

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -11,9 +11,9 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     lazy = true,
     keys = {
-      { "<leader>crj", "<cmd>TSJJoin<CR>",   desc = "Join lines with TS context" },
-      { "<leader>crs", "<cmd>TSJSplit<CR>",  desc = "Split current line with TS context" },
-      { "|",           "<cmd>TSJToggle<CR>", desc = "split if oneline, join if multiline" }
+      { "<leader>cj", "<cmd>TSJJoin<CR>",   desc = "Join lines with TS context" },
+      { "<leader>cs", "<cmd>TSJSplit<CR>",  desc = "Split current line with TS context" },
+      { "|",          "<cmd>TSJToggle<CR>", desc = "split if oneline, join if multiline" }
     },
     opts = {
       use_default_keymaps = false

d8cf425d8bd023f40ff21f75b726d6db6ca4da7e
Matheus Ashton <matheusashton@gmail.com>
2026-04-10 17:56:23 -0300

    [nvim] fixing ´gd´ and ´gr´ bindings

--- nvim/.config/nvim/lua/config/key-mapping.lua
+++ nvim/.config/nvim/lua/config/key-mapping.lua
@@ -1,6 +1,11 @@
 return function()
   vim.keymap.set("n", "s", "<nop>")
   vim.keymap.set("n", "<C-I>", "<Tab>", { noremap = true })
+  vim.keymap.del("n", "gra")
+  vim.keymap.del("n", "gri")
+  vim.keymap.del("n", "grn")
+  vim.keymap.del("n", "grr")
+  vim.keymap.del("n", "grt")
   return {
     { "<bs><bs>", desc = "Clear search highlights" },
     { "<tab>", "%", desc = "go to matching part", mode = { "n" } },
@@ -40,9 +45,8 @@ return function()
     { "]e", "<CMD>lua vim.diagnostic.goto_next()<CR>", icon = "󰮲", desc = "Go to next diagnostic" },
 
     { "g", group = "goto" },
-    { "gd", "<CMD>lua vim.lsp.buf.definition()<CR>", icon = "󰞥", desc = "go to symbol definition" },
-    { "gr", "<CMD>lua vim.lsp.buf.references()<CR>", icon = { "󰘦" }, desc = "Go to symbol references" },
-    { "grr", icon = "" },
+    { "gd", icon = "󰞥" },
+    { "gr", icon = "󰘦" },
 
     { "X", "<CMD>lua vim.diagnostic.open_float()<CR>", desc = "Open diagnostic in a floating window", icon = { "" } },
     { "<C-SPACE>", "<CMD>lua vim.diagnostic.open_float()<CR>", desc = "Open Diagnostic in a floating window", icon = { "󱂬 " } },

6aabc24fd3f0d6732597236347cbed8a8342d53a
Matheus Ashton <matheusashton@gmail.com>
2026-04-10 17:50:17 -0300

    [nvim] organizing dev plugins

--- nvim/.config/nvim/lua/plugins/dev.lua
+++ nvim/.config/nvim/lua/plugins/dev.lua
@@ -1,14 +1,13 @@
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        "neotest",
-        "plenary.nvim",
-        "mini.test"
-      }
-    }
-  },
-  { 'nvim-mini/mini.test', version = false, opts = {} },
+  -- {
+  --   "folke/lazydev.nvim",
+  --   ft = "lua", -- only load on lua files
+  --   opts = {
+  --     library = {
+  --       "neotest",
+  --       "plenary.nvim",
+  --       "mini.test"
+  --     }
+  --   }
+  -- },
 }

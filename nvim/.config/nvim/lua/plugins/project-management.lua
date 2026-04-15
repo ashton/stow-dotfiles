c6316fe1f1492a2932e2d0bc4fbb7bcbde56c6d0
Matheus Ashton <matheusashton@gmail.com>
2026-04-15 18:09:41 -0300

    [nvim] changing project.nvim plugin

--- nvim/.config/nvim/lua/plugins/project-management.lua
+++ nvim/.config/nvim/lua/plugins/project-management.lua
@@ -1,15 +1,32 @@
 return {
   {
-    "ahmedkhalf/project.nvim",
-    event = "VeryLazy",
-    opts = {
-      exclude_dirs = { "~", "~/.cargo/*", }
+    'DrKJeff16/project.nvim',
+    cmd = { -- Lazy-load by commands
+      'Project',
+      'ProjectAdd',
+      'ProjectConfig',
+      'ProjectDelete',
+      'ProjectExport',
+      'ProjectHealth',
+      'ProjectHistory',
+      'ProjectImport',
+      'ProjectRecents',
+      'ProjectRoot',
+      'ProjectSession',
+      'ProjectTelescope', -- If using `telescope.nvim` integration
     },
+    dependencies = {      -- OPTIONAL. Choose any of the following
+      { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+    },
+    opts = {
+      patterns = { ".project" }
+    },
+    config = function(_, opts)
+      require('project').setup(opts)
+      require('telescope').load_extension('projects')
+    end,
     keys = {
       { "<leader>pp", "<cmd>Telescope projects<CR>", desc = "List recent projects" }
     },
-    config = function(_, opts)
-      require("project_nvim").setup(opts)
-    end
-  }
+  },
 }

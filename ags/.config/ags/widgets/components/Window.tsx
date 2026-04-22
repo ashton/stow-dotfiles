0ab6189580484e8715ac96c83b1cb3165463beb1
Matheus Ashton <matheusashton@gmail.com>
2026-04-22 14:24:37 -0300

    [ags] fixing window title when title changes in the same focused client

--- ags/.config/ags/widgets/components/Window.tsx
+++ ags/.config/ags/widgets/components/Window.tsx
@@ -1,16 +1,25 @@
 import { focusedClient } from "../../variables"
-import { With } from "ags";
+import { createBinding, With } from "ags";
 
-const windowName = focusedClient(client => client?.title)
 export default () =>
-  <With value={windowName}>
-    {name => (
-      <box
-        visible={name != null}
-        class="component active-window"
-        spacing={5}>
-        <image iconName="cs-windows" />
-        <label label={name} />
-      </box>
-    )}
+  <With value={focusedClient}>
+    {client => {
+      var bind = createBinding(client, "title");
+      var title = bind(str => {
+        if (str.length > 50)
+          return `${str.slice(0, 50)}...`;
+
+        return str;
+      })
+
+      return (
+        <box
+          visible={title != null}
+          class="component active-window"
+          spacing={5}>
+          <image iconName="cs-windows" />
+          <label label={title} />
+        </box>
+      )
+    }}
   </With>

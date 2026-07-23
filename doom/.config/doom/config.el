2d3b64f592d2df5e6fc665d57367c3e1b7cd427a
Matheus Ashton <matheusashton@gmail.com>
2026-07-23 16:36:01 -0300

    [doom] adding rescript support

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -74,3 +74,13 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(after! rescript-mode
+  (setq lsp-rescript-server-command
+        '("rescript-language-server" "--stdio"))
+  ;; Tell `lsp-mode` about the `rescript-vscode` LSP server
+  (require 'lsp-rescript)
+  ;; Enable `lsp-mode` in rescript-mode buffers
+  (add-hook 'rescript-mode-hook 'lsp-deferred)
+  ;; Enable display of type information in rescript-mode buffers
+  (require 'lsp-ui)
+  (add-hook 'rescript-mode-hook 'lsp-ui-doc-mode))

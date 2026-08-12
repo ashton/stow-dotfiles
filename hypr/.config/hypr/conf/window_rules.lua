331a815c3f42ca5fa17daac4ed0767f2cf4728af
Matheus Ashton <matheusashton@gmail.com>
2026-08-12 18:02:22 -0300

    [hypr] adding window rules names and tagging ghostty code instances

--- hypr/.config/hypr/conf/window_rules.lua
+++ hypr/.config/hypr/conf/window_rules.lua
@@ -1,5 +1,13 @@
-hl.window_rule({ match = { class = "Logseq" }, workspace = "special:notes" })
-hl.window_rule({ match = { class = "ghostty.floatty" }, workspace = "special:terminal" })
-hl.window_rule({ match = { class = "firefox" }, workspace = "name:web" })
-hl.window_rule({ match = { class = "Spotify" }, workspace = "name:music" })
-hl.window_rule({ match = { class = "org.gnupg.pinentry-qt" }, float = true, center = true, pin = true, no_shadow = true, no_blur = true })
+hl.window_rule({ name = "Logseq opens in special:notes", match = { class = "Logseq" }, workspace = "special:notes" })
+hl.window_rule({
+  name = "Floaty terminal opens in special:terminal",
+  match = { class = "ghostty.floatty" },
+  workspace =
+  "special:terminal"
+})
+hl.window_rule({ name = "Browsers open in web workspace", match = { class = "^(firefox|zen)$" }, workspace = "name:web" })
+hl.window_rule({ name = "Spotify opens in music workspace", match = { class = "Spotify" }, workspace = "name:music" })
+hl.window_rule({ name = "Pinentry centered and floating", match = { class = "org.gnupg.pinentry-qt" }, float = true, center = true, pin = true, no_shadow = true, no_blur = true })
+
+hl.window_rule({ name = "Add tag +code to ghostty in code workspace", match = { class = "com.mitchellh.ghostty", workspace = "name:code" }, tag =
+"+code" })
